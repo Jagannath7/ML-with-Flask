44d9be006d18ce0fe9cc64ad58c6441591bbf303
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-10-02 22:29:48 +0530

    trying jinja template

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+my_friends = ['Anne', 'Bill', 'Cathy']
 
 @app.route('/')
 def hello():
-    return "HELLO WORLD"
+    return render_template("index.html", friends = my_friends)
 
+@app.route('/home')
+def home():
+    return redirect('/')
+
+@app.route('/about')
+def about():
+    return "<h1> About Page </h1>"
 
 if __name__ == '__main__':
     #app.debug = True
