d8849b8f8fae3f6c18f1178b689b1ce232670211
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-10-02 22:07:06 +0530

    started flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "HELLO WORLD"
+
+
+if __name__ == '__main__':
+    #app.debug = True
+    app.run(debug=True)
